cca848bb003cf1c813768fd7e5b3677bd09bb1b6
Gomes, J.M <gomesjm@ita.br>
2024-05-05 15:14:15 -0300

    env

--- postgres/init/01-pm.sh
+++ postgres/init/01-pm.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE USER pmdb;
     CREATE DATABASE pmdb;
     ALTER DATABASE pmdb OWNER TO pmdb;
 EOSQL
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "ALTER ROLE pmdb WITH PASSWORD '$POSTGRES_PASSWORD';"
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -c "ALTER ROLE pmdb WITH PASSWORD 'pmdb';"
