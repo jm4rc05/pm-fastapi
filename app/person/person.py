6182eb9e6407e14de53759c0aed09b0e41c9fb7c
Gomes, J.M <gomesjm@ita.br>
2024-05-10 13:28:12 -0300

    cleanup

--- app/person/person.py
+++ app/person/person.py
@@ -14,21 +14,15 @@ from graph.queries import person
 
 api = FastAPI()
 
-if __name__ != "__person__":
-    @api.middleware('http')
-    async def dispatch(request: Request, next):
-        if not is_authorized(request):
-            log = 'Not authorized'
-            logger.error(log)
-            return JSONResponse(content = { 'ERROR': { 'message': log } }, status_code = 401)
+@api.middleware('http')
+async def dispatch(request: Request, next):
+    if not is_authorized(request):
+        log = 'Not authorized'
+        logger.error(log)
+        return JSONResponse(content = { 'ERROR': { 'message': log } }, status_code = 401)
 
-        return await next(request)
+    return await next(request)
 
 api.mount('/person/', GraphQL(person.schema('graph'), debug = True))
 
-if __name__ == "__person__":
-    import uvicorn 
-
-    uvicorn.run("person:api", host="0.0.0.0", reload=True, port=8000)
-else: 
-    handler = Mangum(api, lifespan = 'off')
+handler = Mangum(api, lifespan = 'off')
