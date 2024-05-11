9d8eea666b1d06e4be44e57e1abe6fa08e5ba180
Gomes, J.M <gomesjm@ita.br>
2024-05-11 10:36:37 -0300

    upd

--- run.sh
+++ run.sh
@@ -16,14 +16,14 @@ aws ssm put-parameter --name 'kms-db-key' --value ${kms_db_key_value} --type 'Se
 
 header='{"alg":"HS256","typ":"JWT"}' && payload='{"token": "'${kms_api_key_value}'"}' && encoded_header=$(echo -n "$header" | base64 | tr -d '=' | tr '/+' '_-') && encoded_payload=$(echo -n "$payload" | base64 | tr -d '=' | tr '/+' '_-') && header_payload="${encoded_header}.${encoded_payload}" && signature=$(echo -n "$header_payload" | openssl dgst -sha256 -hmac "${kms_api_key_value}" -binary | base64 | tr -d '=' | tr '/+' '_-')
 
-export authorization_token="${header_payload}.${signature}" && echo Authorization token: $authorization_token
+export authorization_token="${header_payload}.${signature}" && echo 'export authorization_token='$authorization_token
 
 function person() {
     # Setup dependencies
     pipenv requirements > app/person/requirements.txt
 
     # Deploy/test person
-    sls person:deploy --stage local && export endpoint_person=`aws lambda create-function-url-config --function-name person-local-api --auth-type NONE | jq -r '.FunctionUrl'` && echo 'End point: '${endpoint_person}
+    sls person:deploy --stage local && export endpoint_person=`aws lambda create-function-url-config --function-name person-local-api --auth-type NONE | jq -r '.FunctionUrl'` && echo 'export endpoint_person='${endpoint_person}
 
     # Unauthorized
     curl -X POST ${endpoint_person}/person/ -H 'Content-Type: application/json' -d '{"query": "{ persons { name title } }"}'
@@ -36,6 +36,10 @@ function person() {
     # curl -X POST ${endpoint_person}/person/ -H 'Content-Type: application/json' -H 'Authorization: Bearer '${authorization_token} -d '{"query": "mutation { add(name: \"Zé\", title: \"Bocó\") { name, title } }" }'
 
     curl -X POST ${endpoint_person}/person/ -H 'Content-Type: application/json' -H 'Authorization: Bearer '${authorization_token} -d '{"query": "{ persons { name title } }"}'
+
+    curl -X POST ${endpoint_person}/person/ -H 'Content-Type: application/json' -H 'Authorization: Bearer '${authorization_token} -d '{"query": "mutation { update(name: \"Carla\", title: \"PhD Candidate\") { name, title } }" }'
+
+    curl -X POST ${endpoint_person}/person/ -H 'Content-Type: application/json' -H 'Authorization: Bearer '${authorization_token} -d '{"query": "mutation { delete(name: \"Carla\") }" }'
 }
 
 function resource() {
@@ -43,7 +47,7 @@ function resource() {
     pipenv requirements > app/resource/requirements.txt
 
     # Deploy/test resource
-    sls resource:deploy --stage local && export endpoint_resource=`aws lambda create-function-url-config --function-name resource-local-api --auth-type NONE | jq -r '.FunctionUrl'` && echo 'End point: '${endpoint_resource}
+    sls resource:deploy --stage local && export endpoint_resource=`aws lambda create-function-url-config --function-name resource-local-api --auth-type NONE | jq -r '.FunctionUrl'` && echo 'export endpoint_resource='${endpoint_resource}
 
     # Unauthorized
     curl -X POST ${endpoint_resource}/resource/ -H 'Content-Type: application/json' -d '{"query": "{ resources { name description } }"}'
