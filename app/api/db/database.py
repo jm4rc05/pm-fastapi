42bd842e0a48dce6ed169d8d145dac632eb5463c
Gomes, J.M <gomesjm@ita.br>
2024-05-23 18:42:54 -0300

    psycopg

--- app/api/db/database.py
+++ app/api/db/database.py
@@ -14,7 +14,7 @@ postgres_host = config('POSTGRES_HOST')
 postgres_port = config('POSTGRES_PORT')
 postgres_database = config('POSTGRES_DATABASE')
 
-DATABASE_URL = f'postgresql+pg8000://{postgres_user}:{postgres_password}@{postgres_host}:{postgres_port}/{postgres_database}'
+DATABASE_URL = f'postgresql+psycopg://{postgres_user}:{postgres_password}@{postgres_host}:{postgres_port}/{postgres_database}'
 
 engine = create_engine(DATABASE_URL)
 
