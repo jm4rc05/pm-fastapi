d73bc07bc354b2b59022abf91fdc4368dbc6c0b1
Gomes, J.M <gomesjm@ita.br>
2024-05-23 15:19:45 -0300

    logging

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
-import logging
+import os, logging.config
 from fastapi import FastAPI, Request, Response, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
-from fastapi_logger.logger import log_request
+from fastapi_route_logger_middleware import RouteLoggerMiddleware
 from typing import Annotated
 from datetime import datetime, timedelta, timezone
 from ariadne.asgi import GraphQL
@@ -12,10 +12,10 @@ from api.middleware.authorization import user, token, authenticate
 from api.resolvers import person, resource
 
 
-logger = logging.getLogger()
-logger.setLevel(config('LOG_LEVEL', default = 'INFO'))
-
 api = FastAPI()
+
+logging.config.fileConfig(f'{os.path.dirname(os.path.realpath(__file__))}/logging.conf')
+api.add_middleware(RouteLoggerMiddleware)
 
 @api.on_event('startup')
 async def startup():
@@ -38,14 +38,12 @@ async def login(data: Annotated[OAuth2PasswordRequestForm, Depends()]) -> Token:
 persons_app = GraphQL(person.schema(), debug = True)
 
 @api.post('/person')
-@log_request
 async def person(request: Request, user: Annotated[Account, Depends(user)]):
     return await persons_app.http_handler.graphql_http_server(request)
 
 resources_app = GraphQL(resource.schema(), debug = True)
 
 @api.post('/resource')
-@log_request
 async def resource(request: Request, user: Annotated[Account, Depends(user)]):
     return await resources_app.http_handler.graphql_http_server(request)
 
