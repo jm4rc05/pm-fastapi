5920988b69f4b8fefc2a0762d0eff049b6fc889f
Gomes, J.M <gomesjm@ita.br>
2024-05-25 16:26:28 -0300

    key 32

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 function main() {
     echo 'ADMIN_KEY='$(openssl rand -hex 32) > .env.local
     echo 'ADMIN_SALT='$(openssl rand -hex 16) >> .env.local
-    echo 'SECRET_KEY='$(openssl rand -hex 16) >> .env.local
+    echo 'SECRET_KEY='$(openssl rand -hex 32) >> .env.local
 
     pipenv lock && pipenv requirements > requirements.txt
 
